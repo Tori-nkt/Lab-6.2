9ae02e16ffbdddc00ffc5a72978459a2b83505fe
Tori-nkt <Tori-nkt@users.noreply.github.com>
2020-06-06 12:25:49 +0300

    Done

---
+++ ЛР_6.2_СТО/Repair.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ЛР_6._2_СТО
+{
+
+    public class Repair : Diagnostics
+    {
+        bool available = true;
+        int guarantie = 0;
+        int new_days;
+        public Repair()
+        {
+            for (int i = 0; i < 30; i++)
+            {
+
+                Random rand = new Random();
+                int k = rand.Next(0, 4);
+                if (k == 0)
+                {
+                    available = false;
+                    new_days = days + rand.Next(1, 3);
+                }
+                else if (k == 1)
+                {
+                    int d = rand.Next(1, 2);
+                    if (days - d >= 0)
+                    {
+                        available = true;
+                        new_days = days - d;
+                    }
+                    else
+                    {
+                        new_days = days;
+                    }
+                }
+                guarantie = rand.Next(1, 3);
+            }
+            info();
+
+
+        }
+        public override void info()
+        {
+            base.info();
+            Console.WriteLine($"Деталь в наличии: {available}" + "\n" + $"Время на починку(по факту): {new_days} дней" + "\n" + $"Цена(по факту): {price}"
+                + "\n" + $"Гарантия: {guarantie} месяцев");
+        }
+
+    }
+}
---
+++ ЛР_6.2_СТО/all_info.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ЛР_6._2_СТО
+{
+    class all_info<T>
+    {
+        private int allRepairs;
+        private int Diag;
+        private int FullDiag;
+
+        public all_info(List<T> array)
+        {
+            FullDiag = 0;
+            allRepairs = 0;
+            Diag = 0;
+            for (int i = 0; i < array.Count; i++)
+            {
+                string s = array[i].ToString().Split(':', '\n')[1];
+                if (s.Equals(" Поверхностная диагностика"))
+                {
+                    Diag++;
+                }
+                else if (s.Equals(" Полная диагностика + починка"))
+                {
+                    allRepairs++;
+                    FullDiag++;
+                }
+                else if (s.Equals(" Поверхностная диагностика + починка"))
+                {
+                    allRepairs++;
+                    Diag++;
+                }
+                else if (s.Equals(" Полная диагностика"))
+                {
+                    FullDiag++;
+                }
+            }
+            Console.WriteLine("Выполнено работ:");
+            Console.WriteLine($"Поверхностная диагностика: {Diag}" + 
+                "\n" + $"Полная диагностика: {FullDiag}" + "\n" + $"Починка: {allRepairs}");
+        }
+
+    }
+}
